275c842a39f09fdf4b626e988644461971802c89
Felipe Quintero <ffquint@gmail.com>
2021-03-06 12:45:35 -0500

    Successfully fetched data from API. Code displays 6 characters so far. Styled components are being used on several files.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import styled from 'styled-components';
-// import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [ charID, setCharID ] = useState('')
+  const [ characters, setCharacters ] = useState([])
+  const [ currentCharID, setCurrentCharID ] = useState('1')
 
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+      .then(res => {
+        setCharacters(res.data)
+        console.log(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
 
   return (
     <AppDiv>
       <HeaderH1>Characters</HeaderH1>
+      {
+        characters.map(e => {
+          return <Character key={e.name} info={e}/>
+        })
+      }  
+      {
+        currentCharID
+      }
     </AppDiv>
   );
 }
@@ -23,12 +44,14 @@ const App = () => {
 export default App;
 
 const AppDiv = styled.div`
-  // code here
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  justify-content: center;
   text-align: center;
 `;
 
 const HeaderH1 = styled.h1`
-  //code here
   color: #443e3e;
   text-shadow: 1px 1px 5px #fff;
 `;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,27 +1,31 @@
 // Write your Character component here
 import React from 'react'
-// import Axios from 'axios'
 import styled from 'styled-components'
 
 export default function Character(props) {
-    const { character } = props;
+    const { info } = props;
 
     return (
         <CharacterDiv>
             <div>
-                <h3>{character.name}</h3>
-                <div>{character.map((item, index) => <li key={index}></li>)}</div>
+                <h3>{info.name}</h3>
+                <p>{info.birth_year}</p>
             </div>
         </CharacterDiv>
     )
 }
 
 const CharacterDiv = styled.div`
-    background-color: #f4f4f4;
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    justify-content: center;
+    background-color: #f4f4f4
     color: #414b3c;
     border: 1px solid #d2d2d2;
     box-shadow: 0px 1px 6px -2px #807f7f;
     border-radius: 8px;
     margin-top: 16px;
     padding: 16px;
+    max-width: 50%;
 `;
